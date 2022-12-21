24daf138be71b0c3315ee7d04c7162bbfd6029dc
lmtroper <larissatroper@outlook.com>
2022-12-20 19:03:37 -0500

    made services section responsive

--- script.js
+++ script.js
@@ -85,109 +85,139 @@ prev.addEventListener('click', () => {
 })
 
 // MOBILE CAROUSEL
-const trackMobile = document.querySelector('.track-mb');
-let moving = false;
-let lastPosition = 0;
-let transform = 0;
-let lastPageX = 0;
-let transformValue = 0;
+// const trackMobile = document.querySelector('.track-mb');
+// let moving = false;
+// let lastPosition = 0;
+// let transform = 0;
+// let lastPageX = 0;
+// let transformValue = 0;
 
-const gestureStart = (e) => {
-    moving = true;
-    lastPosition = e.pageX;
-    transform = window.getComputedStyle(trackMobile)
-        .getPropertyValue('transform') != 'none' ?
-        window.getComputedStyle(trackMobile)
-        .getPropertyValue('transform').split(',')[4].trim() : 0;
-};
+// const gestureStart = (e) => {
+//     moving = true;
+//     lastPosition = e.pageX;
+//     transform = window.getComputedStyle(trackMobile)
+//         .getPropertyValue('transform') != 'none' ?
+//         window.getComputedStyle(trackMobile)
+//         .getPropertyValue('transform').split(',')[4].trim() : 0;
+// };
 
-const gestureMove = (e) => {
-    if (moving) {
-        const diffX = e.pageX - lastPosition;
+// const gestureMove = (e) => {
+//     if (moving) {
+//         const diffX = e.pageX - lastPosition;
 
-        //Moving from left to right
-        if (e.pageX - lastPageX > 0) {
-            if (transformValue > 0) {
-                return;
-            }
-        } else {
-            if (Math.abs(transformValue) > trackMobile.offsetWidth - 340) {
-                console.log(transformValue);
-                console.log(trackMobile.offsetWidth - 50);
-                return;
-            }
-        }
-        transformValue = parseInt(transform) + diffX;
-        trackMobile.style.transform = `translateX(${transformValue}px)`;
+//         //Moving from left to right
+//         if (e.pageX - lastPageX > 0) {
+//             if (transformValue > 0) {
+//                 return;
+//             }
+//         } else {
+//             if (Math.abs(transformValue) > trackMobile.offsetWidth - 340) {
+//                 console.log(transformValue);
+//                 console.log(trackMobile.offsetWidth - 50);
+//                 return;
+//             }
+//         }
+//         transformValue = parseInt(transform) + diffX;
+//         trackMobile.style.transform = `translateX(${transformValue}px)`;
+//     }
+//     lastPageX = e.pageX; //Track last pages value when mouse moves
+// };
+
+// const gestureEnd = (e) => {
+//     moving = false;
+// };
+
+// if (window.PointerEvent) {
+//     window.addEventListener('pointerdown', gestureStart);
+
+//     window.addEventListener('pointermove', gestureMove);
+
+//     window.addEventListener('pointerup', gestureEnd);
+// } else {
+//     window.addEventListener('touchstart', gestureStart);
+//     window.addEventListener('touchmove', gestureMove);
+//     window.addEventListener('touchend', gestureEnd);
+
+//     window.addEventListener('mousedown', gestureStart);
+//     window.addEventListener('mousemove', gestureMove);
+//     window.addEventListener('mouseup', gestureEnd);
+
+// }
+
+
+// SERVICES SECTION: ACCORDION RESPONSIVENESS
+
+var windowSize = true; //value is true when window > 992px
+console.log('initial size:', windowSize)
+
+window.addEventListener('load', function() {
+    if (this.window.innerWidth < 992) {
+        var active = document.querySelector('.accordion__button--active');
+        active.classList.remove("accordion__button--active")
+        windowSize = false;
     }
-    lastPageX = e.pageX; //Track last pages value when mouse moves
-};
-
-const gestureEnd = (e) => {
-    moving = false;
-};
-
-if (window.PointerEvent) {
-    window.addEventListener('pointerdown', gestureStart);
-
-    window.addEventListener('pointermove', gestureMove);
-
-    window.addEventListener('pointerup', gestureEnd);
-} else {
-    window.addEventListener('touchstart', gestureStart);
-    window.addEventListener('touchmove', gestureMove);
-    window.addEventListener('touchend', gestureEnd);
-
-    window.addEventListener('mousedown', gestureStart);
-    window.addEventListener('mousemove', gestureMove);
-    window.addEventListener('mouseup', gestureEnd);
-
-}
-
-// SERVICES
-const welding = document.querySelector("[id='welding-service']");
-const consulting = document.querySelector("[id='consult-service']");
-const equipRepair = document.querySelector("[id='equip-service']");
-const masonry = document.querySelector("[id='masonry-service']");
-
-const servicesText = document.querySelector("[id='services-text']");
-
-const img = document.querySelector("[id='services-img']");
-
-let activeTab = welding;
-
-welding.addEventListener('click', () => {
-    img.classList = 'welding';
-    servicesText.classList = 'col-5 welding';
-
-    activeTab.style.backgroundColor = '#1E1E23';
-    activeTab = welding;
-    activeTab.style.backgroundColor = '#3b3b45'
 })
 
-consulting.addEventListener('click', () => {
-    img.classList = 'consult';
-    servicesText.classList = 'col-5 consulting'
+window.addEventListener("resize", function() {
+    var active = document.querySelector('.accordion__button--active');
+    // console.log(windowSize)
 
-    activeTab.style.backgroundColor = '#1E1E23';
-    activeTab = consulting;
-    activeTab.style.backgroundColor = '#3b3b45'
-})
+    if (window.innerWidth < 992) {
+        if (windowSize) {
+            console.log('proof 1')
+                // console.log('window < 992 and it came from bigger')
+            active.classList.remove("accordion__button--active")
+            windowSize = false;
+        }
+    } else {
+        if (!windowSize) {
+            // Minimize all the open tabs opened in condensed screen
+            document.querySelectorAll('.accordion__button').forEach(button => {
+                const accordionContent = button.nextElementSibling;
+                accordionContent.style.maxHeight = 0;
+                button.classList.remove('accordion__button--active');
+            });
 
-equipRepair.addEventListener('click', () => {
-    img.classList = 'equip';
-    servicesText.classList = 'col-5 equipment';
+            document.querySelector('[id="tab1"]').classList.add('accordion__button--active');
+            document.querySelector(".service-card--active").classList.remove('service-card--active');
+            document.querySelector("[id='card1']").classList.add('service-card--active')
+        }
+        windowSize = true;
 
-    activeTab.style.backgroundColor = '#1E1E23';
-    activeTab = equipRepair;
-    activeTab.style.backgroundColor = '#3b3b45'
-})
 
-masonry.addEventListener('click', () => {
-    img.classList = 'masonry';
-    servicesText.classList = 'col-5 masonry';
+    };
 
-    activeTab.style.backgroundColor = '#1E1E23';
-    activeTab = masonry;
-    activeTab.style.backgroundColor = '#3b3b45'
+});
+
+document.querySelectorAll('.accordion__button').forEach(button => {
+    button.addEventListener('click', () => {
+        var active = document.querySelector('.accordion__button--active');
+        //console.log(button)
+        if (window.innerWidth < 992) {
+            console.log('window less than 992')
+            const accordionContent = button.nextElementSibling;
+            button.classList.toggle('accordion__button--active');
+
+            if (button.classList.contains('accordion__button--active')) {
+                accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+            } else {
+                accordionContent.style.maxHeight = 0;
+            }
+
+        } else {
+            console.log('clicking button in 992+ screen')
+            let prevButton = document.querySelector(".accordion__button--active");
+            prevButton.classList.remove('accordion__button--active');
+            prevButton = button.classList.add('accordion__button--active');
+
+            const id = button.id.slice(-1)
+
+            let prevCard = document.querySelector(".service-card--active");
+            prevCard.classList.toggle('service-card--active');
+            const card = document.querySelector("[id='card" + id + "']")
+            prevCard = card
+            prevCard.classList.toggle('service-card--active');
+
+        };
+    })
 })
