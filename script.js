22b292eb72e52450cefbc6aae40260a3f538c230
lmtroper <larissatroper@outlook.com>
2022-12-23 21:26:11 -0500

    merged the nav bars into one

--- script.js
+++ script.js
@@ -26,29 +26,6 @@ navToggle.addEventListener('click', () => {
 
 document.getElementById('mobile-header-title').innerHTML = 'WELDING SERVICES\nIN THE GTA <br><button id="services-btn" class="btn px-3 py-2">OUR SERVICES</button><button id="contact-btn" class="btn px-3 py-2">CONTACT US</button>';
 
-// // SERVICE CARD DROPDOWNS
-
-// const serviceCard = document.getElementById("services-btn-weld");
-// // console.log(serviceCard)
-
-// serviceCard.addEventListener('click', () => {
-//     const dropdown = document.getElementById('services-dropdown-weld')
-//     const arrow = document.getElementById('arrow-1')
-//         // console.log(dropdown);
-//     visibility = dropdown.getAttribute('data-visible')
-//     direction = arrow.getAttribute('value')
-
-//     if (visibility === 'false') {
-//         dropdown.setAttribute('data-visible', true);
-//         serviceCard.setAttribute('value', true);
-//         arrow.setAttribute('value', true);
-//     } else {
-//         dropdown.setAttribute('data-visible', false);
-//         serviceCard.setAttribute('value', false);
-//         arrow.setAttribute('value', false);
-
-//     }
-// })
 
 //CAROUSEL
 const prev = document.querySelector('.prev')
@@ -84,65 +61,6 @@ prev.addEventListener('click', () => {
 
 })
 
-// MOBILE CAROUSEL
-// const trackMobile = document.querySelector('.track-mb');
-// let moving = false;
-// let lastPosition = 0;
-// let transform = 0;
-// let lastPageX = 0;
-// let transformValue = 0;
-
-// const gestureStart = (e) => {
-//     moving = true;
-//     lastPosition = e.pageX;
-//     transform = window.getComputedStyle(trackMobile)
-//         .getPropertyValue('transform') != 'none' ?
-//         window.getComputedStyle(trackMobile)
-//         .getPropertyValue('transform').split(',')[4].trim() : 0;
-// };
-
-// const gestureMove = (e) => {
-//     if (moving) {
-//         const diffX = e.pageX - lastPosition;
-
-//         //Moving from left to right
-//         if (e.pageX - lastPageX > 0) {
-//             if (transformValue > 0) {
-//                 return;
-//             }
-//         } else {
-//             if (Math.abs(transformValue) > trackMobile.offsetWidth - 340) {
-//                 console.log(transformValue);
-//                 console.log(trackMobile.offsetWidth - 50);
-//                 return;
-//             }
-//         }
-//         transformValue = parseInt(transform) + diffX;
-//         trackMobile.style.transform = `translateX(${transformValue}px)`;
-//     }
-//     lastPageX = e.pageX; //Track last pages value when mouse moves
-// };
-
-// const gestureEnd = (e) => {
-//     moving = false;
-// };
-
-// if (window.PointerEvent) {
-//     window.addEventListener('pointerdown', gestureStart);
-
-//     window.addEventListener('pointermove', gestureMove);
-
-//     window.addEventListener('pointerup', gestureEnd);
-// } else {
-//     window.addEventListener('touchstart', gestureStart);
-//     window.addEventListener('touchmove', gestureMove);
-//     window.addEventListener('touchend', gestureEnd);
-
-//     window.addEventListener('mousedown', gestureStart);
-//     window.addEventListener('mousemove', gestureMove);
-//     window.addEventListener('mouseup', gestureEnd);
-
-// }
 
 
 // SERVICES SECTION: ACCORDION RESPONSIVENESS
